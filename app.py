df2c9d9f13d7c20b2ae6ffd91b6f2416e81b7969
Nikmal8 <kamalcharan404@gmail.com>
2024-08-02 14:53:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-import numpy as np
-import cv2
-import streamlit as st
-from tensorflow import keras
-from tensorflow.keras.models import model_from_json
+from keras.models import load_model
 from keras.preprocessing.image import img_to_array
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, VideoProcessorBase, WebRtcMode
+import cv2
+import numpy as np
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, WebRtcMode
 
-# load model
-emotion_dict = {0:'angry', 1 :'happy', 2: 'neutral', 3:'sad', 4: 'surprise'}
-# load json and create model
-try:
-    json_file = open('emotion_model1.json', 'r')
-    loaded_model_json = json_file.read()
-    classifier = model_from_json(loaded_model_json)
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classifier = load_model('model.h5')
 
-except Exception as e:
-    st.error(f"Error loading model: {e}")
-# load weights into new model
-classifier.load_weights("emotion_model1.h5")
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
-#load face
+video = cv2.VideoCapture(0)
+
+# load face
 try:
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 except Exception:
@@ -27,17 +20,17 @@ except Exception:
 
 RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
-class Faceemotion(VideoTransformerBase):
+
+class FaceEmotion(VideoTransformerBase):
     def transform(self, frame):
         img = frame.to_ndarray(format="bgr24")
-
-        #image gray
+        # image gray
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(
             image=img_gray, scaleFactor=1.3, minNeighbors=5)
         for (x, y, w, h) in faces:
             cv2.rectangle(img=img, pt1=(x, y), pt2=(
-                x + w, y + h), color=(255, 0, 0), thickness=2)
+                x + w, y + h), color=(255, 100, 0), thickness=2)
             roi_gray = img_gray[y:y + h, x:x + w]
             roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
             if np.sum([roi_gray]) != 0:
@@ -46,25 +39,33 @@ class Faceemotion(VideoTransformerBase):
                 roi = np.expand_dims(roi, axis=0)
                 prediction = classifier.predict(roi)[0]
                 maxindex = int(np.argmax(prediction))
-                finalout = emotion_dict[maxindex]
+                finalout = emotion_labels[maxindex]
                 output = str(finalout)
-            label_position = (x, y)
-            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, output, (x + 6, y - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
 
         return img
 
+
 def main():
     # Face Analysis Application #
-    st.title("Real Time Face Emotion Detection Application")
+    st.title("Emotion Detection Application provide by KAMAL SINGH CHARAN")
     activiteis = ["Home", "Webcam Face Detection", "About"]
     choice = st.sidebar.selectbox("Select Activity", activiteis)
-    st.sidebar.markdown(
-        """ Developed by KAMAL SINGH CHARAN  
-            Email : KAMALCHARAN404@GMAIL.COM""")
+    st.sidebar.markdown('''
+      # About Me \n 
+        Hey this is **KAMAL SINGH CHARAN**. \n
+
+        Also check us out on Social Media
+        - [Portfolio](https://main--nikmal8.netlify.app/)
+        - [LinkedIn](https://www.linkedin.com/in/kamal-charan21/)
+        - [Github](https://github.com/Nikmal8)
+        ''')
+
     if choice == "Home":
-        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
-                                            <h4 style="color:white;text-align:center;">
-                                            Face Emotion detection application using OpenCV, Custom CNN model and Streamlit.</h4>
+        html_temp_home1 = """<div style=padding:10px">
+                                            <h4 style=text-align:center;">
+                                            Emotion detection application using OpenCV, Custom CNN model and Streamlit.
+                                            </h4>
                                             </div>
                                             </br>"""
         st.markdown(html_temp_home1, unsafe_allow_html=True)
@@ -73,34 +74,34 @@ def main():
 
                  1. Real time face detection using web cam feed.
 
-                 2. Real time face emotion recognization.
+                 2. Real time Emotions recognition.
 
                  """)
+
     elif choice == "Webcam Face Detection":
         st.header("Webcam Live Feed")
-        st.write("Click on start to use webcam and detect your face emotion")
+        st.write("Click on start to use webcam and detect your Eomtions")
         webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
-                        video_processor_factory=Faceemotion)
+                        video_processor_factory=FaceEmotion)
 
     elif choice == "About":
         st.subheader("About this app")
-        html_temp_about1= """<div style="background-color:#6D7B8D;padding:10px">
-                                    <h4 style="color:white;text-align:center;">
-                                    Real time face emotion detection application using OpenCV, Custom Trained CNN model and Streamlit.</h4>
+        html_temp_about1 = """<div style=padding:10px">
+                                    <h4 style=text-align:center;">
+                                    Emotions detection application using OpenCV, Custom Trained CNN model and Streamlit.</h4>
                                     </div>
                                     </br>"""
         st.markdown(html_temp_about1, unsafe_allow_html=True)
 
-        html_temp4 = """
-                             		<div style="background-color:#98AFC7;padding:10px">
-                             		<h4 style="color:white;text-align:center;">This Application is developed by KAMAL SINGH CHARAN using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or wnat to comment just write a mail KAMALCHARAN404@GMAIL.COM. </h4>
-                             		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
-                             		</div>
-                             		<br></br>
-                             		<br></br>"""
+        html_temp4 = """ 
+                        <div style=padding:10px">
+                        <h5 style=text-align:center;">This Application is developed by KAMAL SINGH CHARAN using Streamlit, Framework, Opencv, Tensorflow and Keras library for demonstration purpose. If you're on LinkedIn and want to connect, just click on the link in sidebar and shoot me a request. If you have any suggestion or want to comment just write a mail at KC621843@GMAIL.COM. </h5>
+                        <h6 style=text-align:center;">Thanks for Visiting</h6>
+                        </div>
+                        <br></br>
+                        <br></br>"""
 
         st.markdown(html_temp4, unsafe_allow_html=True)
-
     else:
         pass
 
